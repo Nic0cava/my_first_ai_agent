7e37ffba05fc1797023111aebb130f7b78f79baf
Nic0cava <nmcava01@gmail.com>
2026-02-02 18:51:42 -0500

    tested api call with simple prompt

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("API key not found!")
 
+user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents=user_prompt
 )
-
-print(response.text)
+if response.usage_metadata == None:
+    raise RuntimeError("API request failed!")
+print(f"User prompt: {user_prompt}")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response: {response.text}")
 
