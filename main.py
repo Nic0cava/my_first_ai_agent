788f3cad0da33464fdc099bccf30dc8a12a3f035
Nic0cava <nmcava01@gmail.com>
2026-03-20 19:37:30 -0400

    added system_propt variable in prompts.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -20,7 +21,8 @@ messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)]
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=messages
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0)
 )
 if response.usage_metadata == None:
     raise RuntimeError("API request failed!")
