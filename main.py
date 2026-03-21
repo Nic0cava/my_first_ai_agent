35b6cb1e7c9c72b3d774c9e2cbe0d1f3c5b12be7
Nic0cava <nmcava01@gmail.com>
2026-03-20 20:06:18 -0400

    added function declaration

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from functions.call_functions import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -22,14 +23,21 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0)
+    config=types.GenerateContentConfig(tools=[available_functions], 
+                                       system_instruction=system_prompt,
+                                        temperature=0
+                                        )
 )
-if response.usage_metadata == None:
+if response.usage_metadata is None:
     raise RuntimeError("API request failed!")
+
 if args.verbose:
     print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if response.function_calls:
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 else:
     print(f"Response: {response.text}")
-
